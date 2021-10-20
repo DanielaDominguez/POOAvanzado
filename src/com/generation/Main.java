f36df298519f39cd1063965b4edb79340eeb4690
Daniela Dominguez Peña <daniela.dominguezz.p@gmail.com>
2021-10-20 16:40:39 -0500

    try/catch

--- src/com/generation/Main.java
+++ src/com/generation/Main.java
@@ -4,16 +4,38 @@ import com.generation.banco.CuentaBancaria;
 import com.generation.paquete2.Animal;
 
 public class Main {
+    //declarar variables
+    public static int numerador = 10;
+    public static int denominador= 2;
+    public static float division;
 
     public static void main(String[] args) {
 	// write your code here
 
+        System.out.println("Antes de hacer la división");
+
+        try {
+            division = numerador/ denominador;
+        }catch (NullPointerException ex) {
+            System.out.println("NullPointer");
+            System.out.println(ex.getMessage());
+        }catch (Exception ex) {
+            System.out.println("Exception");
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("Soy finally");
+        }
+        System.out.println("Después de la división");
+
+
+        /*
         //Ejercicio encapsulamiento
         Person person = new Person();
         person.setName("Santiago");
         person.setAge(33);
         person.setId("102045455");
         person.mostrarPerson();
+        */
 
 
         //System.out.println( "Name: " + person.getName());
@@ -43,7 +65,6 @@ public class Main {
         persona.nombre = "Daniela";
         persona.edad = 31;
         persona.
-
          */
     }
 }
